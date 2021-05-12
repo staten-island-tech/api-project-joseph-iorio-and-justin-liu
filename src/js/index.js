f15c03e4055d9ee033075650329eee5f16fbfa52
JosephIorio <joseph.iorio21@sitechhs.com>
2021-05-12 08:38:46 -0400

    dsw
    wqwq

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,25 @@
 import { DOMSelectors } from "./DOM";
 import { genres } from "./genre";
 
-const key = "65e83edc8ce1540342e4df24c69e948f";
+var lastfm = new LastFM({
+  apiKey: "c9946d11aaaaaaaaaaaaaaaaaaaaaaaace",
+  apiSecret: "9dabf9aaaaaaaaaaaaaaaaxxx11ec3c7a993",
+  cache: cache,
+});
+
+headers = {
+  "user-agent": USER_AGENT,
+};
+
+payload = {
+  api_key: API_KEY,
+  method: "chart.gettopartists",
+  format: "json",
+};
+
+r = requests.get(
+  "https://ws.audioscrobbler.com/2.0/",
+  (headers = headers),
+  (params = payload)
+);
+r.status_code;
