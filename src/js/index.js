6c256b5e1fac8ef326050e7a1cbce593dea62054
JosephIorio <joseph.iorio21@sitechhs.com>
2021-05-18 08:20:11 -0400

    aaaaaa
    aa

--- src/js/index.js
+++ src/js/index.js
@@ -24,46 +24,46 @@ function httpGetAsync(theUrl, callback) {
     if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
       callback(xmlHttp.responseText);
   };
-  xmlHttp.open("GET", theUrl, true); // true for asynchronous
+  xmlHttp.open("GET", theUrl, true);
   xmlHttp.send(null);
 }
- var topArtistName = '';
-   // get weekly artist chart by tag 'trance'
+var topArtistName = "";
+lastfm.tag.getWeeklyArtistChart(
+  { tag: "pop", limit: 6 },
+  {
+    success: function (data) {
+      $("#top_artists").html(
+        "#lastfmTemplateArtists".render(data.weeklyartistchart.artist)
+      );
 
-	    lastfm.tag.getWeeklyArtistChart({tag: 'pop', limit: 6}, {success: function(data){
+      topArtistName = data.weeklyartistchart.artist[0].name;
 
-	        // render top weekly artist using 'lastfmTemplateArtists' template
+      lastfm.artist.getInfo(
+        { artist: topArtistName },
+        {
+          success: function (data) {
+            $("#top_artist").html(
+              $("#lastfmTemplateArtistInfo").render(data.artist)
+            );
+            var list = "<ol>";
+            for (var i = 0; i < data.topartists.artist.length; i++) {
+              list += "<li>" + data.topartists.artist[i].name + "</li>";
+            }
+            document.getElementById("topartists").innerHTML = list + "</ol>";
 
-	        $('#top_artists').html(
-
-	            ('#lastfmTemplateArtists').render(data.weeklyartistchart.artist)
-
-	        );
-
-	        topArtistName = data.weeklyartistchart.artist[0].name;
-
-	        // load details of the artist
-
-	        lastfm.artist.getInfo({artist: topArtistName}, {success: function(data){
-
-	            // render the single artist info using 'lastfmTemplateArtistInfo' template
-
-	            $('#top_artist').html(
-
-	                $('#lastfmTemplateArtistInfo').render(data.artist)
-
-	            );
-
-	            // load the artis's top tracks
-
-	            lastfm.artist.getTopTracks({artist: topArtistName, limit: 9}, {success: function(data){
-
-	                // render the tracks using 'lastfmTemplateTracks' template
-
-	                $('#top_tracks').html(
-
-	                    $('#lastfmTemplateTracks').render(data.toptracks.track)
-
-	                );
-
-	            }})
+            lastfm.artist.getTopTracks(
+              { artist: topArtistName, limit: 9 },
+              {
+                success: function (data) {
+                  $("#top_tracks").html(
+                    $("#lastfmTemplateTracks").render(data.toptracks.track)
+                  );
+                },
+              }
+            );
+          },
+        }
+      );
+    },
+  }
+);
