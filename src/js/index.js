7ae908d7aa7755786e63bbbe33c2535a62d918a8
JosephIorio <71645078+JosephIorio@users.noreply.github.com>
2021-05-23 17:08:54 -0400

    efewf
    wefrws

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
-import { DOMSelectors } from "./DOM";
-import { genres } from "./genre";
-import { lastfm } from "./lastfm.api.js";
+var Lastfm = new lastFM({
+  apiKey: "f21088bf9097b49ad4e7f487abab981e",
+  apiSecret: "7ccaec2093e33cded282ec7bc81c6fca",
+  cache: cache,
+});
 
 lastfm.substring(1);
 
@@ -21,75 +23,66 @@ function httpGetAsync(theUrl, callback) {
   xmlHttp.send(null);
 }
 var topArtistName = "";
-lastfm.tag.getWeeklyArtistChart(
-  { tag: "pop", limit: 6 },
-  {
-    success: function (data) {
-      $("#top_artists").html(
-        "#lastfmTemplateArtists".render(data.weeklyartistchart.artist)
-      );
+{
+  topArtistName = data.weeklyartistchart.artist[0].name;
+  window.document.onload = lastfm.user.getTopArtists(
+    {
+      user: "dior-_-",
+      limit: 10,
+    },
+    {
+      success: function (data) {},
+      error: function (data) {
+        alert(data.error + " " + data.message);
+      },
+    }
+  );
+  $(document).ready(function () {
+    $.getJSON(
+      "http://ws.audioscrobbler.com/2.0/?method=user.getTopArtists&user=test&api_key=4a9f5581a9cdf20a699f540ac52a95c9&limit=10&format=json&callback=?",
+      function (json) {
+        var html = "";
+        $.each(json.topartists.artist, function (i, item) {
+          html +=
+            "<p><a href=" +
+            item.url +
+            " target='_blank'>" +
+            item.name +
+            " - " +
+            "Play count : " +
+            item.playcount +
+            "</a></p>";
+        });
+        $("#result").append(html);
+      }
+    );
+  });
 
-      topArtistName = data.weeklyartistchart.artist[0].name;
-      window.document.onload = lastfm.user.getTopArtists(
-        {
-          user: "dior-_-",
-          limit: 10,
-        },
-        {
-          success: function (data) {},
-          error: function (data) {
-            alert(data.error + " " + data.message);
-          },
+  lastfm.artist.getInfo(
+    { artist: topArtistName },
+    {
+      success: function (data) {
+        console.log(data);
+        $("#top_artist").html(
+          $("#lastfmTemplateArtistInfo").render(data.artist)
+        );
+        var list = "<ol>";
+        for (var i = 0; i < data.topartists.artist.length; i++) {
+          list += "<li>" + data.topartists.artist[i].name + "</li>";
         }
-      );
-      $(document).ready(function () {
-        $.getJSON(
-          "http://ws.audioscrobbler.com/2.0/?method=user.getTopArtists&user=test&api_key=4a9f5581a9cdf20a699f540ac52a95c9&limit=10&format=json&callback=?",
-          function (json) {
-            var html = "";
-            $.each(json.topartists.artist, function (i, item) {
-              html +=
-                "<p><a href=" +
-                item.url +
-                " target='_blank'>" +
-                item.name +
-                " - " +
-                "Play count : " +
-                item.playcount +
-                "</a></p>";
-            });
-            $("#result").append(html);
+        document.getElementById("topartists").innerHTML = list + "</ol>";
+
+        lastfm.artist.getTopTracks(
+          { artist: topArtistName, limit: 9 },
+          {
+            success: function (data) {
+              $("#top_tracks").html(
+                $("#lastfmTemplateTracks").render(data.toptracks.track)
+              );
+            },
           }
         );
-      });
-
-      lastfm.artist.getInfo(
-        { artist: topArtistName },
-        {
-          success: function (data) {
-            console.log(data);
-            $("#top_artist").html(
-              $("#lastfmTemplateArtistInfo").render(data.artist)
-            );
-            var list = "<ol>";
-            for (var i = 0; i < data.topartists.artist.length; i++) {
-              list += "<li>" + data.topartists.artist[i].name + "</li>";
-            }
-            document.getElementById("topartists").innerHTML = list + "</ol>";
-
-            lastfm.artist.getTopTracks(
-              { artist: topArtistName, limit: 9 },
-              {
-                success: function (data) {
-                  $("#top_tracks").html(
-                    $("#lastfmTemplateTracks").render(data.toptracks.track)
-                  );
-                },
-              }
-            );
-          },
-        }
-      );
-    },
-  }
-);
+      },
+    }
+  );
+}
